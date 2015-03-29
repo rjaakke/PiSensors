e8289704767067aad8fe42e7fd5f734b55869792
Robert Jaakke <robert@jaakke.com>
2015-03-29 09:27:35 +0200

    Reverted change

--- EnergySensor/app.js
+++ EnergySensor/app.js
@@ -29,58 +29,60 @@ App.prototype = {
   watch: function(callback) {
     if (callback && typeof(callback) === "function") {
       this.onSensorEvent = callback;
-      this.sensor.watch(function(err, state) {
-        var currentEvent = moment();
-
-        var currentInterval = currentEvent.diff(this.previousEvent);
-        var intervalFactor = this.previousInterval / currentInterval;
-        var date = new Date();
-        var currentRate = date.getHours() <= 7 ? 1 : 2; //Rate 1 is Low and 2 is High
-
-        if (currentInterval > 30) {
-          if (intervalFactor < 1 && this.firstEvent === false) {
-
-            //TODO: 375 should be the C var
-            var currentWatt = (3600000 / (375 * currentInterval) * 1000);
-
-            this.logger.debug('State = ' + state + ' Another roundtrip, time: ' +
-              currentEvent.format() + ' interval : ' + currentInterval +
-              'ms factor : ' + intervalFactor + ' rate is: ' + currentRate +
-              ' current usage:' + currentWatt);
-
-            this.onSensorEvent({
-              sensorId: this.sensorInfo.sensorId,
-              time: currentEvent.valueOf(),
-              rate: currentRate
-            }, function(err) {
-              if (err instanceof Error) {
-                this.logger.warm('Event logging failed!');
-              }
-            });
-
-          } else if (this.firstEvent === true) {
-            this.firstEvent = false;
-            this.logger.debug('Skipping first event to avoid getting noise in the results');
-
-          } else {
-            this.logger.debug('End of the black stripe, we can drop this event: ' +
-              currentInterval + 'ms');
-          }
-
-          this.previousEvent = currentEvent;
-          this.previousInterval = currentInterval;
-        } else {
-
-          this.logger.debug('Singal bounce ignored, interval: ' + currentInterval + 'ms');
-        }
-      });
+      this.sensor.watch(sensorEventHandler);
     } else {
       this.logger.warn('No callback supplied, aborted watching!');
     }
   },
 
   unwatch: function() {
-    this.sensor.unwatchAll();
+    this.sensor.unwatch(sensorEventHandler);
+  },
+
+  sensorEventHandler: function(err, state) {
+    var currentEvent = moment();
+
+    var currentInterval = currentEvent.diff(this.previousEvent);
+    var intervalFactor = this.previousInterval / currentInterval;
+    var date = new Date();
+    var currentRate = date.getHours() <= 7 ? 1 : 2; //Rate 1 is Low and 2 is High
+
+    if (currentInterval > 30) {
+      if (intervalFactor < 1 && this.firstEvent === false) {
+
+        //TODO: 375 should be the C var
+        var currentWatt = (3600000 / (375 * currentInterval) * 1000);
+
+        this.logger.debug('State = ' + state + ' Another roundtrip, time: ' +
+          currentEvent.format() + ' interval : ' + currentInterval +
+          'ms factor : ' + intervalFactor + ' rate is: ' + currentRate +
+          ' current usage:' + currentWatt);
+
+        this.onSensorEvent({
+          sensorId: this.sensorInfo.sensorId,
+          time: currentEvent.valueOf(),
+          rate: currentRate
+        }, function(err) {
+          if (err instanceof Error) {
+            this.logger.warm('Event logging failed!');
+          }
+        });
+
+      } else if (this.firstEvent === true) {
+        this.firstEvent = false;
+        this.logger.debug('Skipping first event to avoid getting noise in the results');
+
+      } else {
+        this.logger.debug('End of the black stripe, we can drop this event: ' +
+          currentInterval + 'ms');
+      }
+
+      this.previousEvent = currentEvent;
+      this.previousInterval = currentInterval;
+    } else {
+
+      this.logger.debug('Singal bounce ignored, interval: ' + currentInterval + 'ms');
+    }
   }
 };
 
